89c727d4463d4d018fe537cf97f5d5c638297d20
snigupta <snigupta@amd.com>
2026-03-25 11:57:20 -0600

    Standardize driver use
    Signed-off-by: snigupta <snigupta@amd.com>

--- profile/device/aie_trace/client/aie_trace_offload_client.h
+++ profile/device/aie_trace/client/aie_trace_offload_client.h
@@ -28,8 +28,13 @@
 
 
 extern "C" {
-  #include <aie_codegen.h>
-  #include <aie_codegen_inc/xaiegbl_params.h>
+#ifdef XDP_USE_AIE_CODEGEN
+#include <aie_codegen.h>
+#include <aie_codegen_inc/xaiegbl_params.h>
+#else
+#include <xaiengine.h>
+#include <xaiengine/xaiegbl_params.h>
+#endif
 }
 
 namespace xdp {

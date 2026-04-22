b3ff8fffeacb773334537fd02bf9913f7be4363f
snigupta <snigupta@amd.com>
2026-04-22 13:16:34 -0600

    Update headers and CMAKE for aie_base and aie_debug
    Signed-off-by: snigupta <snigupta@amd.com>

--- profile/plugin/aie_base/aie_base_util.h
+++ profile/plugin/aie_base/aie_base_util.h
@@ -11,8 +11,13 @@
 #include "xdp/profile/plugin/aie_base/generations/aie_generations.h"
 
 extern "C" {
+#ifdef XDP_USE_AIE_CODEGEN
+#include <aie_codegen.h>
+#include <aie_codegen_inc/xaiegbl_params.h>
+#else
 #include <xaiengine.h>
 #include <xaiengine/xaiegbl_params.h>
+#endif
 }
 
 namespace xdp::aie {
--- profile/plugin/aie_debug/ve2/aie_debug.h
+++ profile/plugin/aie_debug/ve2/aie_debug.h
@@ -18,8 +18,13 @@
 #include "xaiefal/xaiefal.hpp"
 
 extern "C" {
+#ifdef XDP_USE_AIE_CODEGEN
+#include <aie_codegen.h>
+#include <aie_codegen_inc/xaiegbl_params.h>
+#else
   #include <xaiengine.h>
   #include <xaiengine/xaiegbl_params.h>
+#endif
 }
 
 namespace xdp {
